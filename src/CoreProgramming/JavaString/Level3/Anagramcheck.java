776886178eb70e4fa79c30503d649cbfa4596dfe
varshanbumani <varshanbu13@gmail.com>
2025-12-19 10:52:04 +0530

    Added Anagram check program

---
+++ src/CoreProgramming/JavaString/Level3/Anagramcheck.java
@@ -0,0 +1,26 @@
+package CoreProgramming.JavaString.Level3;
+import java.util.Scanner;
+public class Anagramcheck {
+    public static boolean isAnagram(String a, String b) {
+        if (a.length() != b.length()) return false;
+
+        int[] freq = new int[256];
+        for (int i = 0; i < a.length(); i++) {
+            freq[a.charAt(i)]++;
+            freq[b.charAt(i)]--;
+        }
+
+        for (int f : freq)
+            if (f != 0) return false;
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println("Anagram? " + isAnagram(a, b));
+    }
+}
